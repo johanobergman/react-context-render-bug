3a4b096b8f876aa7821e4969f7c791b70ccfff35
Johan Bergman <johan.bergman1@gmail.com>
2018-01-04 14:39:27 +0100

    Simpler example

--- src/index.js
+++ src/index.js
@@ -3,52 +3,32 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import PropTypes from 'prop-types'
 
+let deepChild = null
+
 class App extends React.Component {
-  broadcast = null
+  static childContextTypes = {}
 
-  static contextTypes = {
-    broadcast: PropTypes.object,
-  }
-
-  static childContextTypes = {
-    broadcast: PropTypes.object.isRequired,
-  }
-
-  getChildContext() {
-    return {
-      broadcast: this.broadcast,
-    }
-  }
-
-  componentWillMount() {
-    this.broadcast = {
-      // Will be set by the <Listener /> child,
-      // would be an array in reality.
-      listener: null,
-    }
-  }
-
-  notifyChild = () => {
-    // The deeply nested child <Listener /> will receive a call
-    // which will cause it to update its state.
+  updateAppAndDeepChild = () => {
+    // The deeply nested child <DeepChild /> will receive a force update.
     // We'll also trigger an update from App, but this should be blocked
-    // by <Blocker />.
+    // at <Blocker />.
     // Despite this, <LoggerWhichShouldNotUpdate />, which is a direct
-    // child of <Blocker /> still get's updated - but only if the listener
-    // below is called so that <Listener /> updates itself.
+    // child of <Blocker /> still get's updated - but only as long as
+    // <DeepChild /> is updated within the same tick and <App />
+    // defines childContextTypes.
     this.forceUpdate()
-    this.broadcast.listener(Math.random())
+    deepChild.forceUpdate()
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.notifyChild}>
-          Notify &lt;Listener /&gt; with new number
+        <button onClick={this.updateAppAndDeepChild}>
+          Force update &lt;App /&gt; and &lt;DeepChild /&gt;
         </button>
         <Blocker>
           <LoggerWhichShouldNotUpdate>
-            <Listener />
+            <DeepChild />
           </LoggerWhichShouldNotUpdate>
         </Blocker>
       </div>
@@ -73,43 +53,36 @@ class LoggerWhichShouldNotUpdate extends React.Component {
     // Does not get called
     console.log('LoggerWhichShouldNotUpdate#willReceiveProps')
   }
+
   componentWillUpdate() {
     // Gets called
     console.log('LoggerWhichShouldNotUpdate#willUpdate')
   }
+
   render() {
     // Called despite parent being a Blocker
     console.log('LoggerWhichShouldNotUpdate#render')
     return (
       <div style={{ border: '2px solid #eee', padding: 10 }}>
         &lt;LoggerWhichShouldNotUpdate /&gt;, blocked by parent
-        shouldComponentUpdate. Render called {this.timesRenderCalled++} times
+        shouldComponentUpdate, render called {this.timesRenderCalled++} times
         {this.props.children}
       </div>
     )
   }
 }
 
-class Listener extends React.Component {
-  static contextTypes = {
-    broadcast: PropTypes.object.isRequired,
-  }
-
-  state = {
-    number: 1,
-  }
+class DeepChild extends React.Component {
+  timesRenderCalled = 0
 
   componentDidMount() {
-    this.context.broadcast.listener = number => {
-      this.setState({ number })
-    }
+    deepChild = this
   }
 
   render() {
     return (
       <div style={{ border: '2px solid #eee', padding: 10 }}>
-        &lt;Listener /&gt; number via context injected listener:{' '}
-        {this.state.number}
+        &lt;DeepChild /&gt;, render called {this.timesRenderCalled++} times
       </div>
     )
   }
